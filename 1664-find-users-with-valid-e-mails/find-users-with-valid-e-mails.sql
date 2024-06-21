045e5b3df672f0550e1e783411f7a3fd816a4563
Menna Mohamed <90659196+menna7634@users.noreply.github.com>
2024-06-21 20:38:22 +0300

    Time: 997 ms (89.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+
+select * from users where mail  like '[A-Za-z]%' 
+and mail like '%[^0-9._-]%'and mail not like '%[^A-Za-z0-9._-]%@leetcode.com'
+and mail like '%@leetcode.com'
+
