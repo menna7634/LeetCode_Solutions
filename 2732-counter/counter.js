d609f7cfebf3bda3f832f20ceb133d50e8e4f41f
Menna Mohamed <90659196+menna7634@users.noreply.github.com>
2024-06-30 02:25:45 +0300

    Time: 43 ms (91.41%) | Memory: 48.2 MB (92.56%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,15 @@
+
+
+var createCounter = function(n) {
+    let count = n;
+    return function() {
+        return count++;
+    };
+};
+
+
+  const counter = createCounter(10)
+  counter() // 10
+  counter() // 11
+  counter() // 12
+ 
