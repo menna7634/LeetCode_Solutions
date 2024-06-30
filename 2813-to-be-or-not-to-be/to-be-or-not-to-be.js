f172c6a5982b96b7bd9ba6b7939d4644261c01fd
Menna Mohamed <90659196+menna7634@users.noreply.github.com>
2024-07-01 02:16:58 +0300

    Time: 51 ms (65.06%) | Memory: 48.9 MB (40.34%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,24 @@
+
+
+var expect = function(val) {
+    return {
+        toBe:(n)=>{
+     if(val===n)
+     return true;
+     else 
+    throw new Error ("Not Equal");
+        },
+        notToBe:(n)=>{
+            if(val === n)
+             throw new Error ("Equal");
+                  else
+            return true;
+             
+    }
+    }
+};
+
+
+  //expect(5).toBe(5); // true
+  //expect(5).notToBe(5); // throws "Equal"
+ 
