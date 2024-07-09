f735c85e5f8b4b48634f068958f226658df449d5
Menna Mohamed <90659196+menna7634@users.noreply.github.com>
2024-07-09 23:59:23 +0300

    Time: 153 ms (51.90%) | Memory: 76.5 MB (58.31%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        double sum=customers[0][0];
+        double sum=customers[0][0]; // 1
         double w=0;
         for ( int i=0 ; i<customers.size() ; i++)
         {
-                  if(customers[i][0]<sum){
-                sum+=customers[i][1];
-                w+=(sum-customers[i][0]);
+                  if(customers[i][0]<sum){ 
+                sum+=customers[i][1]; // 3+5=8     8+3=11
+                w+=(sum-customers[i][0]); // 2+(8-2)=8    8+(11-4)=15
             }else{
-                sum=customers[i][0]+customers[i][1];
-                w+=customers[i][1];
+                sum=customers[i][0]+customers[i][1]; // 1+2=3
+                w+=customers[i][1]; // 0+2=2
             }
         }
-        return w/customers.size();
+        return w/customers.size(); //15/3
     }
 };
 
