1a71cf5353f193501eec9fda5241bfffbee37fd6
Menna Mohamed <90659196+menna7634@users.noreply.github.com>
2025-11-27 13:16:58 +0200

    Time: 0 ms (100.00%) | Memory: 14.3 MB (58.42%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> st1(nums1.begin(), nums1.end());
-        set<int> st2(nums2.begin(), nums2.end());
-
-   
+      unordered_set<int> st(nums1.begin(), nums1.end());
     vector<int>res;
-    for (int s : st1)
+    for (int i : nums2)
     {
-        if (st2.find(s)!=st2.end())
-        {
-            res.push_back(s);
-        }
+       if (st.count(i)) {
+            res.push_back(i);
+            st.erase(i); 
+        } 
     }
     return res;
     }
